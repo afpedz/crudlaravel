40ab0666d6ee01430c25ca150c88e539a9e869c0
afpedz <75974066+afpedz@users.noreply.github.com>
2025-02-27 15:45:13 +0800

    Update units.blade.php

--- resources/views/units.blade.php
+++ resources/views/units.blade.php
@@ -16,9 +16,9 @@
         <table class="w-full border-collapse border border-gray-300 shadow-lg rounded-lg overflow-hidden">
             <thead>
                 <tr class="bg-gray-800 text-white">
-                    <th class="border border-gray-300 px-5 py-2">ID</th>
-                    <th class="border border-gray-300 px-5 py-2">Name</th>
-                    <th class="border border-gray-300 px-5 py-2">Action</th>
+                    <th class="border border-gray-300 px-5 py-2 w-1/6">ID</th>
+                    <th class="border border-gray-300 px-5 py-2 w-2/3">Name</th>
+                    <th class="border border-gray-300 px-5 py-2 w-1/6">Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -26,30 +26,33 @@
                     @foreach ($units->sortBy('id') as $unit)
                         <tr class="border border-gray-300 text-gray-700 hover:bg-gray-50 item"
                             data-id="{{ $unit->id }}">
-                            <td class="border border-gray-300 px-5 py-2 text-center">{{ $unit->id }}</td>
-                            <td class="border border-gray-300 px-5 py-2">{{ $unit->name }}</td>
-                            <td class="flex justify-center gap-3 px-5 py-2 text-center">
+                            <td class="border border-gray-300 px-5 py-2 text-center w-1/6">{{ $unit->id }}</td>
+                            <td class="border border-gray-300 px-5 py-2 w-2/3">{{ $unit->name }}</td>
+                            <td class="border border-gray-300 px-5 py-2 text-center w-1/6">
                                 <!-- Edit Button -->
-                                <button onclick="showEditModal({{ $unit->id }}, '{{ $unit->name }}')"
-                                    class="text-gray-800 hover:underline">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                    </svg>
-                                </button>
-
-                                |
-
-                                <!-- Delete Button -->
-                                <button onclick="showDeleteModal({{ $unit->id }})"
-                                    class="text-red-500 hover:underline">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                    </svg>
-                                </button>
+                                <div class="flex justify-center gap-3">
+                                    <!-- Edit Button -->
+                                    <button onclick="showEditModal({{ $unit->id }}, '{{ $unit->name }}')"
+                                        class="text-gray-800 hover:underline">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </button>
+        
+                                    <span>|</span>
+        
+                                    <!-- Delete Button -->
+                                    <button onclick="showDeleteModal({{ $unit->id }})"
+                                        class="text-red-500 hover:underline">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                    </button>
+                                </div>
                             </td>
                         </tr>
                     @endforeach
