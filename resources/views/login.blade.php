7fad9140b6be23caf47676428558db27f6f85611
afpedz <75974066+afpedz@users.noreply.github.com>
2025-02-28 00:14:00 +0800

    Added Indention in Category Dropdown

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,31 +8,37 @@
             <form id="loginForm" class="space-y-3" action="{{ route('login') }}" method="post">
                 @csrf
                 @if (session('status'))
-                <div class="bg-red-500 px-4 py-2 rounded-lg mb-6 text-white text-center mt-2">
-                    {{ session('status') }}
-                </div>
+                    <div class="bg-red-500 px-4 py-2 rounded-lg mb-6 text-white text-center mt-2">
+                        {{ session('status') }}
+                    </div>
                 @endif
                 <div>
                     <label class="block text-gray-600">Email</label>
-                    <input type="email" name="email" id="email" class="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 @error('email') border-red-500 @enderror" placeholder="Enter your email">
+                    <input type="email" name="email" id="email"
+                        class="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 @error('email') border-red-500 @enderror"
+                        placeholder="Enter your email">
                     @error('email')
-                    <div class="text-red-500 mt-2 text-sm">
-                        {{ $message }}
-                    </div>
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
                     @enderror
                 </div>
                 <div>
                     <label class="block text-gray-600">Password</label>
-                    <input type="password" name="password" id="password" class="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 @error('password') border-red-500 @enderror" placeholder="Enter your password">
+                    <input type="password" name="password" id="password"
+                        class="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 @error('password') border-red-500 @enderror"
+                        placeholder="Enter your password">
                     @error('password')
-                    <div class="text-red-500 mt-2 text-sm">
-                        {{ $message }}
-                    </div>
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
                     @enderror
                 </div>
-                <button type="submit" class="w-full px-4 py-2 text-white bg-gray-800 rounded-lg hover:bg-gray-900">{{ $title }}</button>
+                <button type="submit"
+                    class="w-full px-4 py-2 text-white bg-gray-800 rounded-lg hover:bg-gray-900">{{ $title }}</button>
             </form>
-            <p class="text-sm text-center text-gray-600">Don't have an account? <a href="/register" class="text-gray-600 hover:underline font-semibold">Sign up</a></p>
+            <p class="text-sm text-center text-gray-600">Don't have an account? <a href="/register"
+                    class="text-gray-600 hover:underline font-semibold">Sign up</a></p>
         </div>
     </div>
     <script>
@@ -41,13 +47,13 @@
                 e.preventDefault(); // Prevent the default form submission
 
                 $.ajax({
-                    type: 'POST'
-                    , url: $(this).attr('action')
-                    , data: $(this).serialize()
-                    , success: function(response) {
+                    type: 'POST',
+                    url: $(this).attr('action'),
+                    data: $(this).serialize(),
+                    success: function(response) {
                         window.location.href = response.redirect;
-                    }
-                    , error: function(xhr) {
+                    },
+                    error: function(xhr) {
                         // Clear previous error messages
                         $('.text-red-500').remove();
 
@@ -57,17 +63,21 @@
                             $.each(errors, function(key, value) {
                                 const input = $('[name="' + key + '"]');
                                 input.addClass('border-red-500'); // Add error class
-                                input.after('<div class="text-red-500 mt-2 text-sm">' + value[0] + '</div>'); // Show error message
+                                input.after('<div class="text-red-500 mt-2 text-sm">' +
+                                    value[0] + '</div>'); // Show error message
                             });
                         } else if (xhr.status === 401) {
                             // Handle invalid login credentials
-                            const statusMessage = xhr.responseJSON.message || 'Invalid login credentials';
+                            const statusMessage = xhr.responseJSON.message ||
+                                'Invalid login credentials';
 
                             // Remove any existing invalid login credentials message
                             $('#loginForm .bg-red-500').remove();
 
                             // Add the new error message
-                            $('#loginForm').prepend('<div class="bg-red-500 px-4 py-2 rounded-lg mb-6 text-white text-center mt-2">' + statusMessage + '</div>');
+                            $('#loginForm').prepend(
+                                '<div class="bg-red-500 px-4 py-2 rounded-lg mb-6 text-white text-center mt-2">' +
+                                statusMessage + '</div>');
                         }
                     }
                 });
@@ -79,7 +89,6 @@
                 $(this).next('.text-red-500').remove(); // Remove the error message
             });
         });
-
     </script>
 
 </x-layout>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -9,8 +9,26 @@
     </x-slot>
 
     <x-slot:onclick>
-        document.getElementById('addProductModal').classList.remove('hidden');
+        showAddProductModal()
     </x-slot>
+
+    @php
+        function renderCategories($categories, $parentId = null, $depth = 0)
+        {
+            foreach ($categories as $category) {
+                if ($category->parent_id == $parentId) {
+                    echo '<option value="' .
+                        $category->id .
+                        '">' .
+                        str_repeat('&nbsp;&nbsp;', $depth) .
+                        $category->name .
+                        '</option>';
+                    renderCategories($categories, $category->id, $depth + 1);
+                }
+            }
+        }
+    @endphp
+
 
     <div class="max-w-7xl mx-auto mt-6">
         @if ($products->isEmpty())
@@ -40,9 +58,24 @@
                             <td class="border border-gray-300 px-5 py-2">{{ $product->price }}</td>
                             <td class="border border-gray-300 px-5 py-2">{{ $product->unit->name }}</td>
                             <td class="border border-gray-300 px-5 py-2">
-                                <button onclick="openModal('{{ $product->id }}')" class="text-gray-800 hover:underline">Edit</button>
-                                |
-                                <button onclick="openConfirmDelete('{{ $product->id }}')" class="text-red-500 hover:underline">Delete</button>
+                                <div class="flex justify-center gap-2">
+                                    <button onclick="openModal('{{ $product->id }}')"
+                                        class="text-gray-800 hover:underline"><svg xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </button>|
+                                    <button onclick="openConfirmDelete('{{ $product->id }}')"
+                                        class="text-red-500 hover:underline"><svg xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                    </button>
+                                </div>
                             </td>
                         </tr>
                     @endforeach
@@ -52,45 +85,54 @@
     </div>
 
     {{-- Product Modal --}}
-    <div id="addProductModal" class="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center z-50 hidden">
+    <div id="addProductModal"
+        class="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center z-50 hidden">
         <div class="bg-white p-8 rounded-lg w-1/4">
             <h2 class="text-2xl font-bold text-center mb-4">Add Product</h2>
             <form id="addProductForm" action="{{ route('products.store') }}" method="POST">
                 @csrf
                 <div class="mb-4">
                     <label for="product_code" class="block text-sm font-medium text-gray-700">Product Code</label>
-                    <input type="text" name="product_code" id="productCode" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md" required />
+                    <input type="text" name="product_code" id="productCode"
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md" required />
                 </div>
                 <div class="mb-4">
                     <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
-                    <textarea name="description" id="productDescription" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md resize-none" rows="2" required></textarea>
+                    <textarea name="description" id="productDescription"
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md resize-none" rows="2" required></textarea>
                 </div>
                 <div class="mb-4">
                     <label for="category" class="block text-sm font-medium text-gray-700">Category</label>
-                    <select name="category_id" id="productCategory" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md" required>
-                    @foreach ($categories as $category)
-                            <option value="{{ $category->id }}">{{ $category->name }}</option>
-                        @endforeach
+                    <select name="category_id" id="productCategory"
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md" required>
+                        <option value="">Select a Category</option>
+                        @php renderCategories($categories) @endphp
                     </select>
                 </div>
 
                 <div class="mb-4">
                     <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
-                    <input type="number" step="0.01" name="price" id="productPrice" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md" required />
+                    <input type="number" step="0.01" name="price" id="productPrice"
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md" required />
                 </div>
 
                 <div class="mb-4">
                     <label for="unit" class="block text-sm font-medium text-gray-700">Unit</label>
-                    <select name="unit_id" id="productUnit" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md" required>
-                    @foreach ($units as $unit)
+                    <select name="unit_id" id="productUnit"
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md" required>
+                        <option value="">Select a Unit</option>
+                        @foreach ($units as $unit)
                             <option value="{{ $unit->id }}">{{ $unit->name }}</option>
                         @endforeach
                     </select>
                 </div>
 
                 <div class="flex justify-between gap-4">
-                    <button type="button" onclick="closeAddProductModal()" class="px-4 py-2 bg-gray-600 text-white rounded-md hover:bg-gray-700 w-full">Cancel</button>
-                    <button type="submit" class="px-4 py-2 bg-gray-800 text-white rounded-md hover:bg-gray-900 w-full">Add Product</button>
+                    <button type="button" onclick="closeAddProductModal()"
+                        class="px-4 py-2 bg-gray-600 text-white rounded-md hover:bg-gray-700 w-full">Cancel</button>
+                    <button type="submit"
+                        class="px-4 py-2 bg-gray-800 text-white rounded-md hover:bg-gray-900 w-full">Add
+                        Product</button>
                 </div>
             </form>
         </div>
@@ -104,6 +146,25 @@
         function closeAddProductModal() {
             document.getElementById('addProductModal').classList.add('hidden');
         }
+
+        document.getElementById("productCategory").addEventListener("change", function() {
+            let select = this;
+            let selectedOption = select.options[select.selectedIndex];
+            if (!selectedOption.hasAttribute("data-original-text")) {
+                selectedOption.setAttribute("data-original-text", selectedOption.textContent);
+            }
+            selectedOption.textContent = selectedOption.getAttribute("data-original-text").trim();
+        });
+
+        document.getElementById("productCategory").addEventListener("mousedown", function() {
+            let select = this;
+            for (let i = 0; i < select.options.length; i++) {
+                let option = select.options[i];
+                if (option.hasAttribute("data-original-text")) {
+                    option.textContent = option.getAttribute("data-original-text"); // Restore indent
+                }
+            }
+        });
     </script>
 
 </x-layout>
